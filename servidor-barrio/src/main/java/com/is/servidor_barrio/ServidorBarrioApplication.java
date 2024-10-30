e29015e19090b2060705a7d5ea728ad808190830
MartinFarres <martinfarres@gmail.com>
2024-10-30 17:35:00 -0300

    ENV management

--- servidor-barrio/src/main/java/com/is/servidor_barrio/ServidorBarrioApplication.java
+++ servidor-barrio/src/main/java/com/is/servidor_barrio/ServidorBarrioApplication.java
@@ -2,11 +2,13 @@ package com.is.servidor_barrio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class ServidorBarrioApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
 		SpringApplication.run(ServidorBarrioApplication.class, args);
 	}
 
