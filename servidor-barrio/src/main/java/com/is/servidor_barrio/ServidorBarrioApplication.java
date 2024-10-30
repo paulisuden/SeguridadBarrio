f143855fb171e72dbcf4c5556104de1dfd30b61b
paulisuden <spaulinaabril@gmail.com>
2024-10-30 17:07:36 -0300

    arreglo

---
+++ servidor-barrio/src/main/java/com/is/servidor_barrio/ServidorBarrioApplication.java
@@ -0,0 +1,13 @@
+package com.is.servidor_barrio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServidorBarrioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServidorBarrioApplication.class, args);
+	}
+
+}
---
+++ servidor-barrio/src/test/java/com/is/servidor_barrio/ServidorBarrioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.is.servidor_barrio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorBarrioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
