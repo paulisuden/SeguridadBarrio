e6c2decdb732fba440a0e7f51a02d02be0f7f731
MartinFarres <martinfarres@gmail.com>
2024-10-31 20:00:07 -0300

    Empleado/PlanillaHoraria/Habitante - Controllers

---
+++ servidor-barrio/src/main/java/com/is/servidor_barrio/business/controllers/EmpleadoController.java
@@ -0,0 +1,15 @@
+package com.is.servidor_barrio.business.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.is.servidor_barrio.business.domain.entity.Empleado;
+import com.is.servidor_barrio.business.logic.service.EmpleadoServiceImpl;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping(path = "/api/empleado")
+public class EmpleadoController extends BaseControllerImpl<Empleado, EmpleadoServiceImpl> {
+
+}
