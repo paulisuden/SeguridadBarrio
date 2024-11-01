498e9afdeeb63493448a2709101c5871db96dc90
paulisuden <spaulinaabril@gmail.com>
2024-11-01 12:27:50 -0300

    envio correo

---
+++ servidor-barrio/src/main/java/com/is/servidor_barrio/business/controllers/MailController.java
@@ -0,0 +1,26 @@
+package com.is.servidor_barrio.business.controllers;
+
+import com.is.servidor_barrio.business.domain.entity.Habitante;
+import com.is.servidor_barrio.business.domain.entity.Visitante;
+import com.is.servidor_barrio.business.logic.service.EnvioCorreoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MailController {
+
+    private final EnvioCorreoService emailService;
+
+    @Autowired
+    public MailController(EnvioCorreoService emailService) {
+        this.emailService = emailService;
+    }
+
+    @GetMapping("/send-email")
+    public String sendEmail(@RequestParam Habitante habitante, @RequestParam Visitante visitante) {
+        emailService.sendEmail(habitante, visitante);
+        return "Email enviado a " + habitante.getApellido() + ", " + habitante.getNombre();
+    }
+}
