d1c94ed818155e73b14a472464336b142ecad2ec
MartinFarres <martinfarres@gmail.com>
2024-11-13 20:22:25 -0300

    Small Fixes of merge

--- servidor-barrio/src/main/java/com/is/servidor_barrio/business/logic/service/ContactoServiceImpl.java
+++ servidor-barrio/src/main/java/com/is/servidor_barrio/business/logic/service/ContactoServiceImpl.java
@@ -1,5 +1,6 @@
 package com.is.servidor_barrio.business.logic.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.is.servidor_barrio.business.domain.entity.Contacto;
