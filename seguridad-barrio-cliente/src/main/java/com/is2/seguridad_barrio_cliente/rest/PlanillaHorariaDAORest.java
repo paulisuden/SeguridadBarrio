4aae1942bac2a4659decce316e5751bb6f3d5cae
paulisuden <spaulinaabril@gmail.com>
2024-11-13 12:39:41 -0300

    planilla horaria

---
+++ seguridad-barrio-cliente/src/main/java/com/is2/seguridad_barrio_cliente/rest/PlanillaHorariaDAORest.java
@@ -0,0 +1,88 @@
+package com.is2.seguridad_barrio_cliente.rest;
+
+//import com.is2.seguridad_barrio_cliente.dto.EmpleadoDTO;
+import com.is2.seguridad_barrio_cliente.dto.PlanillaHorariaDTO;
+import com.is2.seguridad_barrio_cliente.error.ErrorServiceException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class PlanillaHorariaDAORest {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public void crear(PlanillaHorariaDTO planillaHoraria) throws ErrorServiceException {
+        try {
+
+            String uri = "http://localhost:8081/api/planilla";
+            restTemplate.postForEntity(uri, planillaHoraria, PlanillaHorariaDTO.class);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new ErrorServiceException("Error de Sistemas");
+        }
+    }
+
+    public PlanillaHorariaDTO buscar(Long id) throws ErrorServiceException {
+
+        try {
+
+            String uri = "http://localhost:8081/api/planilla/" + id;
+
+            ResponseEntity<PlanillaHorariaDTO> response = restTemplate.getForEntity(uri, PlanillaHorariaDTO.class);
+            PlanillaHorariaDTO planillaHoraria = response.getBody();
+
+            return planillaHoraria;
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new ErrorServiceException("Error de Sistemas");
+        }
+    }
+
+    public void actualizar(PlanillaHorariaDTO planillaHoraria) throws ErrorServiceException {
+
+        try {
+
+            String uri = "http://localhost:8081/api/planilla/" + planillaHoraria.getId();
+            restTemplate.put(uri, planillaHoraria);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new ErrorServiceException("Error de Sistemas");
+        }
+    }
+
+    public void eliminar(Long id) throws ErrorServiceException {
+
+        try {
+
+            String uri = "http://localhost:8081/api/planilla/" + id;
+            restTemplate.delete(uri);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new ErrorServiceException("Error de Sistemas");
+        }
+    }
+
+    public List<PlanillaHorariaDTO> listar() throws ErrorServiceException {
+        try {
+            String uri = "http://localhost:8081/api/planilla";
+
+            ResponseEntity<PlanillaHorariaDTO[]> response = restTemplate.getForEntity(uri, PlanillaHorariaDTO[].class);
+            PlanillaHorariaDTO[] planillasHorarias = response.getBody();
+            List<PlanillaHorariaDTO> listaPlanillaHorarias = Arrays.asList(planillasHorarias);
+            return listaPlanillaHorarias;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new ErrorServiceException("Error de Sistemas");
+        }
+    }
+
+}
---
+++ seguridad-barrio-cliente/src/main/java/com/is2/seguridad_barrio_cliente/service/PlanillaHorariaService.java
@@ -0,0 +1,109 @@
+package com.is2.seguridad_barrio_cliente.service;
+
+import com.is2.seguridad_barrio_cliente.dto.PlanillaHorariaDTO;
+import com.is2.seguridad_barrio_cliente.enumeration.EstadoAsistencia;
+import com.is2.seguridad_barrio_cliente.error.ErrorServiceException;
+//import com.is2.seguridad_barrio_cliente.rest.EmpleadoDAORest;
+import com.is2.seguridad_barrio_cliente.rest.PlanillaHorariaDAORest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.Date;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class PlanillaHorariaService {
+
+    @Autowired
+    private PlanillaHorariaDAORest dao;
+
+    public void crear(LocalDateTime entrada, LocalDateTime salida, EstadoAsistencia estadoAsistencia, String observacion) throws ErrorServiceException {
+
+        try {
+
+            PlanillaHorariaDTO planillaHoraria = new PlanillaHorariaDTO();
+            planillaHoraria.setEntrada(entrada);
+            planillaHoraria.setEstadoAsistencia(estadoAsistencia);
+            planillaHoraria.setObservacionAsistencia(observacion);
+            planillaHoraria.setSalida(salida);
+            //planillaHoraria.setEmpleadoId(idEmpleado);
+
+            dao.crear(planillaHoraria);
+
+        } catch (ErrorServiceException e) {
+            throw e;
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new ErrorServiceException("Error de Sistemas");
+        }
+    }
+
+    public PlanillaHorariaDTO buscar(Long id) throws ErrorServiceException {
+
+        try {
+
+            if (id == 0) {
+                throw new ErrorServiceException("Debe indicar la planilla horaria");
+            }
+
+            PlanillaHorariaDTO planillaHoraria = dao.buscar(id);
+
+            return planillaHoraria;
+
+        } catch (ErrorServiceException ex) {
+            throw ex;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new ErrorServiceException("Error de sistema");
+        }
+    }
+
+    public void modificar(Long id, LocalDateTime entrada, LocalDateTime salida, EstadoAsistencia estadoAsistencia, String observacion) throws ErrorServiceException {
+
+        try {
+
+            PlanillaHorariaDTO planillaHoraria = new PlanillaHorariaDTO();
+            planillaHoraria.setId(id);
+            planillaHoraria.setEntrada(entrada);
+            planillaHoraria.setEstadoAsistencia(estadoAsistencia);
+            planillaHoraria.setObservacionAsistencia(observacion);
+            planillaHoraria.setSalida(salida);
+            //planillaHoraria.setEmpleadoId(idEmpleado);
+
+            dao.actualizar(planillaHoraria);
+
+        } catch (ErrorServiceException e) {
+            throw e;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new ErrorServiceException("Error de Sistemas");
+        }
+    }
+
+    public void eliminar(Long id) throws ErrorServiceException {
+
+        try {
+
+            dao.eliminar(id);
+
+        } catch (ErrorServiceException ex) {
+            throw ex;
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new ErrorServiceException("Error de sistema");
+        }
+
+    }
+
+    public List<PlanillaHorariaDTO> listar() throws ErrorServiceException {
+        try {
+            return dao.listar();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new ErrorServiceException("Error de sistema");
+        }
+    }
+}
